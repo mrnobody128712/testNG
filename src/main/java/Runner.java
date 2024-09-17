88cf59a9243aa5c69137bc5e6b97d4bf10d79cff
Mr. Nobody <mrnobody128712@gmail.com>
2024-09-17 22:24:09 +0300

    v6

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -4,7 +4,8 @@ public class Runner {
         System.out.println("versiyon 1");
         System.out.println("versiyon 2");
         System.out.println("versiyon 3");
-        //remote da değişiklik 
+        //remote da değişiklik
+        System.out.println("developer 1 in commiti");
 
     }
 }
